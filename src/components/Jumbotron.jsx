76a397572b326497afbbc8b9c5727f2f7da9f2f1
DeepGandhi07 <deepgandhi9887@gmail.com>
2023-04-11 16:03:29 +0530

    Commit

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import I14 from "../assets/images/iphone-14.jpg";
+import Holdiphone from "../assets/images/iphone-hand.png";
 const Jumbotron = () => {
   return (
     <div className="jumbotron-section wrapper">
       <h2 className="title">New</h2>
       <img className="logo" src={I14} alt="iphone 14 pro" />
-      <p className="text">Big and Bigger </p>
+      <p className="text">Big and Bigger.</p>
+      <span className="description">
+        From $41.62/mo. for 24 mo. or $999 before trade-in
+      </span>
+      <u1 className="links">
+        <li>
+          <button className="button">Buy</button>
+        </li>
+        <li>
+          <a className="link">Learn more</a>
+        </li>
+      </u1>
+
+      <img className="iphone-img" src={Holdiphone} />
     </div>
   );
 };
---
+++ src/components/Soundsec.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Soundsec = () => {
+  return <div>Soundsec</div>;
+};
+
+export default Soundsec;
